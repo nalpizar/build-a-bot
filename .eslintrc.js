ca6ae73b632b9699a03d31ec629c58b57c8efbe2
Natalia Alpizar <natalpizarm@gmail.com>
2025-03-12 14:49:02 +0100

    feat: add of Composition and Reactivity API

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:vue/essential', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'vue/no-multiple-template-root': 'off',
+  },
   parserOptions: {
     parser: '@babel/eslint-parser',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'linebreak-style': 0,
-    'operator-linebreak': 0,
-    'comma-dangle': ['error', 'always-multiline'],
-    'prettier/prettier': 'error',
   },
 };
